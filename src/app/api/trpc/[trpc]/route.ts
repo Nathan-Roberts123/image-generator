6ae0a2af741be2fe0a71d23e28eaf701df086bca
Pieter Stols <robertsnathan2004@gmail.com>
2024-06-11 08:39:19 +0200

    set maximum duration

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,7 +2,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server/routers";
 import { createContext } from "@/context";
 
-//max maximum duration for request to 3 minutes
+//max maximum duration for a function to proccess a request to 3 minutes
 export const maxDuration = 60 * 3;
 
 const handler = (req: Request) =>
