d2ed40556f43344b43d8b86a4a5e324903d2d67b
Pieter Stols <robertsnathan2004@gmail.com>
2026-06-14 15:51:04 +0200

    Changed OpenAI model to gpt-image-2

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -8,12 +8,30 @@ export async function generateImage(promt: TPrompt) {
   });
 
   const response = await openai.images.generate({
-    model: "dall-e-3",
+    model: "gpt-image-2",
     prompt: promt.text,
     n: 1,
     size: "1024x1024",
     response_format: "b64_json",
   });
 
-  return response.data[0].b64_json;
+  const image_base64 = response.data[0].b64_json;
+
+  return image_base64;
+
+  // const imageUrl = response.data[0].url;
+  //
+  // if (!imageUrl) {
+  //   throw new
+  // }
+  //
+  // // Fetch the image file and convert it into a base64 string
+  // const imageResponse = await fetch(imageUrl);
+  // const arrayBuffer = await imageResponse.arrayBuffer();
+  // const buffer = Buffer.from(arrayBuffer);
+  // const base64Image = buffer.toString("base64");
+  //
+  // const b64JsonOutput = `data:image/png;base64,${base64Image}`;
+  //
+  // return b64JsonOutput;
 }
